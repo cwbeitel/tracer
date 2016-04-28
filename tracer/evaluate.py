0004a6d0dbebe86188bd8480c58f6d64b704f670
cb01 <chris.w.beitel@gmail.com>
2016-04-27 21:02:38 -0700

    Evaluate decodings using edit distance
    Simple script that returns an average, length-weighted edit distance to
    compare a decoding to a key, each having one sequence per line.

--- tracer/evaluate.py
+++ tracer/evaluate.py
@@ -1,33 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
-import click
+import tensorflow as tf
 from Levenshtein import distance
 
-@click.command()
-@click.option("--translations_dir", help="A directory containing a set of files holding inferred translations to be scored along with the correct translations of these.")
-@click.option("--stats_output_path", help="The path to which to write the computed quality statistics.")
+tf.app.flags.DEFINE_string("decoded", "/tmp", "A file containing decoded traces in DNA sequence form.")
+tf.app.flags.DEFINE_string("key", "/tmp", "A file containing correct decodings in DNA sequence form.")
+tf.app.flags.DEFINE_string("score_file", "/tmp", "A file to which to write the computed score.")
 
+FLAGS = tf.app.flags.FLAGS
 
-tf.app.flags.DEFINE_string("trace_path", "/tmp", "Path to a trace to decode.")
-
-
-def cli(translations_dir, stats_output_path):
+def cli():
     """Evaluate a set of basecalls made on an evaluation dataset."""
     score = 0
     ct = 0
-    # For each file stem in the translations_dir directory,
-        # if there is both a .trans.txt and .key.txt file present
-            # read in the contents of both into 'trans' and 'key'
-            trans = 
-            key = 
-            score += 2*distance(trans, key)/(len(trans)+len(key))
-            ct +=1
-    print "the score is " + str(score/ct) + "\n"
-
+    with open(FLAGS.decoded, "r") as decoded_fh:
+        with open(FLAGS.key, "r") as key_fh:
+            d, k = decoded_fh.readline().strip(), key_fh.readline().strip()
+            while d and k:
+                mxlen = max(len(d),len(k))
+                num = float(mxlen - distance(d, k))
+                score += num/max(len(d),len(k))
+                ct +=1
+                d, k = decoded_fh.readline().strip(), key_fh.readline().strip()
+    with open(FLAGS.score_file, "w") as score_fh:
+        score_fh.write("decoded: " + FLAGS.decoded + "\n" + "key: " + FLAGS.key + "\n" + "score: " + str(score/ct) + "\n")
 
 '''
 tracer-eval --inputCalls=$TRACERDIR/data/model.txt --inputKey=$TRACERDIR/data/test_data.txt --output=$TRACERDIR/data/qual.txt
 '''
-
